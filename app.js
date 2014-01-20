12a9f862d3a027b9c96f1497f6e16d3b7d014f86
post-l <ludovic.post@epitech.eu>
2014-01-20 16:13:16 +0100

    add users account

--- app.js
+++ app.js
@@ -1,30 +1,59 @@
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
+var mongoose = require('mongoose');
+var passport = require('passport');
 var path = require('path');
 
 var app = express();
-
 app.set('port', process.env.PORT || 3000);
+app.engine('.html', require('ejs').__express);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
+
+var config = require('./config/config')[app.get('env')];
+
+/**
+ * MongoDB init (connect, models...)
+ */
+
+mongoose.connect('mongodb://localhost/croquerlemonde');
+require('./models/user');
+require('./config/passport')(passport, config);
+
+/**
+ * Basic Middlewares
+ */
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(express.cookieParser('bluebirdisthekey'));
-app.use(express.session());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+/**
+ * Auth Middleware
+ */
+
+app.use(express.cookieParser(config.app.secretKey));
+app.use(express.session());
+app.use(passport.initialize());
+app.use(passport.session());
+
+/**
+ * Router Middlewares
+ */
+
+app.use(app.router);
+app.use(express.compress());
+app.use(express.static(path.join(__dirname, 'public')));
+require('./config/routes')(app, passport, config);
+
+/**
+ * Create http server
+ */
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
---
+++ models/user.js
@@ -0,0 +1,92 @@
+var mongoose = require('mongoose');
+var hash = require('../utils/hash');
+
+UserSchema = mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    salt: String,
+    hash: String,
+    facebook: {
+        id: String,
+        email: String,
+        name: String
+    },
+    google: {
+        id: String,
+        email: String,
+        name: String
+    }
+});
+
+UserSchema.statics.signup = function(email, password, done) {
+    var User = this;
+    hash(password, function(err, salt, hash) {
+        if (err) throw err;
+        // if (err) return done(err);
+        User.create({
+            email: email,
+            salt: salt,
+            hash: hash
+        }, function(err, user) {
+            if(err) throw err;
+            // if (err) return done(err);
+            done(null, user);
+        });
+    });
+};
+
+
+UserSchema.statics.isValidUserPassword = function(email, password, done) {
+    this.findOne({email: email}, function(err, user) {
+        // if (err) throw err;
+        if (err) return done(err);
+        if (!user) return done(null, false, {message: 'Incorrect email.'});
+        hash(password, user.salt, function(err, hash) {
+            if (err) return done(err);
+            if (hash == user.hash) return done(null, user);
+            done(null, false, {message : 'Incorrect password'});
+        });
+    });
+};
+
+UserSchema.statics.findOrCreateOAuthUser = function(profile, done) {
+    var User = this;
+    var query = {};
+    query[profile.authOrigin + '.id'] = profile.id;
+    User.findOne(query, function(err, user) {
+        if (err) throw err;
+        if (user) {
+            done(null, user);
+        } else {
+            User.findOne({ 'email' : profile.emails[0].value }, function(err, user){
+                if (err) throw err;
+                if (user) {
+                    user[''+profile.authOrigin] = {};
+                    user[''+profile.authOrigin].id = profile.id;
+                    user[''+profile.authOrigin].email = profile.emails[0].value;
+                    user[''+profile.authOrigin].name = profile.displayName;
+                    user.save(function(err, user) {
+                        if (err) throw err;
+                        done(null, user);
+                    });
+                } else {
+                    user = {
+                        email : profile.emails[0].value,
+                        firstName : profile.displayName.split(" ")[0],
+                        lastName : profile.displayName.replace(profile.displayName.split(" ")[0] + " ", "")
+                    };
+                    user[''+profile.authOrigin] = {};
+                    user[''+profile.authOrigin].id = profile.id;
+                    user[''+profile.authOrigin].email = profile.emails[0].value;
+                    user[''+profile.authOrigin].name = profile.displayName;
+                    User.create(user, function(err, user) {
+                        if (err) throw err;
+                        done(null, user);
+                    });
+                }});
+        }});
+};
+
+var User = mongoose.model("User", UserSchema);
+module.exports = User;
